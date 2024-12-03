07ad24741612a53cdb9a62d668a8201bb3acf211
大多和磨幸 <23jn0311@jec.ac.jp>
2024-12-03 11:04:56 +0900

    メインメニューのログインを上にしてヘッダーを追加

---
+++ User/利用者/header.php
@@ -0,0 +1,24 @@
+<header>
+    <link href="css/header.css" rel="stylesheet" >
+    <link rel="stylesheet" href="bootstrap-5.0.0-dist/css/bootstrap.min.css">
+    <script src="bootstrap-5.0.0-dist/js/bootstrap.min.js"></script>
+    <div id="logo">
+        <a href="" >
+            <img src="img/オススメイカー画像なし.jpg" alt="a" style="max-height: 60px;">
+        </a>
+    </div>
+    <div id="link">
+
+        <?php if(isset($member)) : ?>
+            <?=$member->membername ?>さん
+            
+            <a href="logout.php">ログアウト</a>
+
+        <?php else: ?>
+            <form action="login.php" method="GET" style="display: inline;">
+                <input type="submit" class="btn btn-primary " value="ログイン">
+            </form>
+
+        <?php endif; ?>
+    </div> 
+</header>
