20ec46d27970663087ed5dcf17bce14e9c6cf276
大多和磨幸 <23jn0311@jec.ac.jp>
2024-12-17 16:34:11 +0900

    3択が多分完成

--- User/利用者/2択.php
+++ User/利用者/2択.php
@@ -2,43 +2,42 @@
 session_start();
 
 // DAO を読み込む
-require_once './help/DAO.php';
+require_once './UserDAO/DAO.php';
 
-if (!isset($_SESSION['current_question_id'])) {
-    $_SESSION['current_question_id'] = intval(DAO::getFirstQuestionId());
-}
 
-$current_question_id = $_SESSION['current_question_id'];
+$current_question_id = isset($_SESSION['current_question_id']) ? $_SESSION['current_question_id'] : DAO2::getFirstQuestionId();
 $answer = isset($_POST['answer']) ? $_POST['answer'] : null;
 
 // 回答をもとに次の質問を取得
-if ($answer) {
-    if ($answer == 'yes') {
-        $next_category_question_id = DAO::getCategoryQuestionId($current_question_id, true);
+
+    if ($answer === 'yes') {
+        $next_category_question_id = DAO2::getCategoryQuestionId($current_question_id, true);
 
         if ($next_category_question_id) {
             $_SESSION['current_question_id'] = $next_category_question_id;
-            $next_question_text = DAO::getCategoryQuestionById($next_category_question_id);
+            $next_question_text = DAO2::getCategoryQuestionById($next_category_question_id);
+            $_SESSION['yes_cid'] = DAO2::getYesCID($next_category_question_id);
         } else {
-            $next_question_id = DAO::getNextQuestionId($current_question_id, true);
+            $next_question_id = DAO2::getNextQuestionId($current_question_id, true);
             $_SESSION['current_question_id'] = $next_question_id;
-            $next_question_text = DAO::getQuestionById($next_question_id);
+            $next_question_text = DAO2::getQuestionById($next_question_id);
         }
-    } else {
-        $next_category_question_id = DAO::getCategoryQuestionId($current_question_id, false);
+    } else if($answer==='no') {
+        $next_category_question_id = DAO2::getCategoryQuestionId($current_question_id, false);
 
         if ($next_category_question_id) {
             $_SESSION['current_question_id'] = $next_category_question_id;
-            $next_question_text = DAO::getCategoryQuestionById($next_category_question_id);
+            $next_question_text = DAO2::getCategoryQuestionById($next_category_question_id);
+            $_SESSION['no_cid'] = DAO2::getNoCID($next_category_question_id);
         } else {
-            $next_question_id = DAO::getNextQuestionId($current_question_id, false);
+            $next_question_id = DAO2::getNextQuestionId($current_question_id, false);
             $_SESSION['current_question_id'] = $next_question_id;
-            $next_question_text = DAO::getQuestionById($next_question_id);
+            $next_question_text = DAO2::getQuestionById($next_question_id);
         }
+    }else{
+        $next_question_text = DAO2::getQuestionById($current_question_id);
     }
-} else {
-    $next_question_text = DAO::getQuestionById($current_question_id);
-}
+
 
 // 質問がない場合はセッションをリセット
 if ($next_question_text == null) {
@@ -57,35 +56,10 @@ if ($next_question_text == null) {
     <link href="css/2choice.css" rel="stylesheet">
     <link rel="stylesheet" href="../bootstrap-5.0.0-dist/css/bootstrap.min.css">
     <script src="../bootstrap-5.0.0-dist/js/bootstrap.min.js"></script>
-</header>
-<body>
-    <div class="game-container">
-        <!-- 背景画像 -->
-        <img src="img/冒険.jpeg" alt="背景画像" class="background-image">
-        
-        <!-- ドラクエ風のメッセージボックス -->
-        <div class="message-box">
-            <div class="question-container">
-                <p id="question-text" class="question"><?php echo $next_question_text; ?></p> <!-- ここで質問文を表示 -->
-            </div>
-
-            <div class="answer-container">
-                <form method="POST" action="" id="answer-form">
-                    <!-- 「はい」と「いいえ」の選択肢をドラクエ風のボックス内に配置 -->
-                    <div class="answer-box">
-                        <a href="#" class="answer-link" id="yes-link" onclick="submitAnswer('yes', event);">
-                            <p>はい</p>
-                        </a>
-                        <a href="#" class="answer-link" id="no-link" onclick="submitAnswer('no', event);">
-                            <p>いいえ</p>
-                        </a>
-                    </div>
-                    <input type="hidden" name="answer" id="answer">
-                </form>
-            </div>
-        </div>
-    </div>
-    <script>
+     <script>
+        window.onbeforeunload = function() {
+            document.body.style = ''; // bodyタグのインラインスタイルをリセット
+        };
         // 文字を一文字ずつ出すアニメーション
         window.addEventListener("load", function() {
             const questionText = document.getElementById("question-text");
@@ -102,15 +76,56 @@ if ($next_question_text == null) {
             }, 100); // 100msごとに1文字ずつ表示
         });
 
-        // ボタンが押された時にフォームを送信して次の質問に進む
         function submitAnswer(answer, event) {
-            event.preventDefault(); // ページ遷移を防ぐ
+        event.preventDefault(); // ページ遷移を防ぐ
 
-            // フォームの hidden 値を設定
-            document.getElementById('answer').value = answer;
-            // フォームを送信
+        // フォームの hidden 値を設定
+        document.getElementById('answer').value = answer;
+
+        // YesCID または NoCID をフォームに設定
+        <?php if (isset($_SESSION['yes_cid']) && $_SESSION['yes_cid'] !== null): ?>
+            if (answer === 'yes') {
+                document.getElementById('cid').value = <?php echo $_SESSION['yes_cid']; ?>;
+            }
+        <?php endif; ?>
+
+        <?php if (isset($_SESSION['no_cid']) && $_SESSION['no_cid'] !== null): ?>
+            if (answer === 'no') {
+                document.getElementById('cid').value = <?php echo $_SESSION['no_cid']; ?>;
+            }
+        <?php endif; ?>
             document.getElementById('answer-form').submit();
         }
     </script>
+</header>
+<body>
+    <div class="game-container">
+        <!-- 背景画像 -->
+        <img src="img/冒険.jpeg" alt="背景画像" class="background-image">
+        
+        <!-- ドラクエ風のメッセージボックス -->
+        <div class="message-box">
+            <div class="question-container">
+                <p id="question-text" class="question"><?php echo $next_question_text; ?></p> <!-- ここで質問文を表示 -->
+            </div>
+
+            <div class="answer-container">
+                <form method="POST" action="kekka.php" id="answer-form">
+                    <!-- 「はい」と「いいえ」の選択肢をドラクエ風のボックス内に配置 -->
+                    <div class="answer-box">
+                    <button type="button" class="answer-button" id="yes-button" onclick="submitAnswer('yes', event);">
+                            はい
+                        </button>
+                        <button type="button" class="answer-button" id="no-button" onclick="submitAnswer('no', event);">
+                            いいえ
+                        </button>
+                    </div>
+                    <input type="hidden" name="answer" id="answer">
+                    <input type="hidden" name="cid" id="cid">
+                </form>
+            </div>
+        </div>
+    </div>
+   
 </body>
 </html>
--- User/利用者/UserDAO/Connect.php
+++
@@ -1,5 +0,0 @@
-<?php
-    // DB接続設定
-    define('DSN', 'sqlsrv:server=JNSV01\sotsu;database=23jn03_G03');
-    define('DB_USER', '23jn03_G03');
-    define('DB_PASSWORD', '23jn03_G03');
